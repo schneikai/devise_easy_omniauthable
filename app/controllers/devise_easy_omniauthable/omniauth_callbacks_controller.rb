06e3a2a4c45758b247b7764445972fb448c93b38
Kai Schneider <schneikai@gmail.com>
2014-07-16 14:14:27 +0200

    refactor. make things more like Devise is doing stuff.

--- app/controllers/devise_easy_omniauthable/omniauth_callbacks_controller.rb
+++ app/controllers/devise_easy_omniauthable/omniauth_callbacks_controller.rb
@@ -22,8 +22,20 @@ class DeviseEasyOmniauthable::OmniauthCallbacksController < Devise::OmniauthCall
       after_sign_in_path_for(resource)
     end
 
+    # Build a devise resource passing in the session. Useful to move
+    # temporary session data to the newly created user.
+    def build_resource(hash=nil)
+      self.resource = resource_class.new_with_session(hash || {}, session)
+    end
+
+    # Signs in a user on sign up. You can overwrite this method in your own
+    # OmniauthCallbacksController.
+    def sign_up(resource_name, resource)
+      sign_in(resource_name, resource)
+    end
+
     def handle_provider(name)
-      omni = request.env["omniauth.auth"]
+      omniauth_data = request.env["omniauth.auth"]
       provider = DeviseEasyOmniauthable::Providers.find_by_name(name)
 
       # First try to find a user with a authentication matching the provider and
@@ -31,36 +43,37 @@ class DeviseEasyOmniauthable::OmniauthCallbacksController < Devise::OmniauthCall
       # user is currently signed in here. In that case the current user is signed
       # out and the other user becomes the current user. That way we make sure
       # that a provider and uid combination matches only one user..
-      resource = resource_class.find_for_omniauth(omni['provider'], omni['uid'])
+      self.resource = resource_class.find_for_omniauth(omniauth_data['provider'], omniauth_data['uid'])
 
       if resource
         sign_in resource
         redirect_to after_sign_in_path_for(resource), notice: I18n.t('devise.omniauth_callbacks.success', kind: provider.human_name)
 
       elsif user_signed_in?
-        current_user.create_or_update_authentication!(omni)
+        current_user.create_or_update_authentication!(omniauth_data)
         redirect_to after_sign_in_path_for(current_user), notice: I18n.t('devise.omniauth_callbacks.success', kind: provider.human_name)
 
-      elsif (email = omni['extra']['raw_info'].email).present? && resource = resource_class.find_for_omniauth_by_email(email)
-        resource.create_or_update_authentication!(omni)
+      elsif (email = omniauth_data['extra']['raw_info'].email).present? && self.resource = resource_class.find_for_omniauth_by_email(email)
+        resource.create_or_update_authentication!(omniauth_data)
 
         sign_in resource
         redirect_to after_sign_in_path_for(resource), notice: I18n.t('devise.omniauth_callbacks.success', kind: provider.human_name)
 
       else
-        resource = resource_class.new
-        resource.apply_omniauth(omni)
-        resource.skip_confirmation! # TODO: Make this configurable?
+        # Create a new user.
+        build_resource({})
+        resource.apply_omniauth(omniauth_data)
+        resource.skip_confirmation! if resource.respond_to?('skip_confirmation!') # TODO: Make this configurable?
 
         if resource.save
-          sign_in resource
+          sign_up(resource_name, resource)
           redirect_to after_sign_up_path_for(resource), notice: I18n.t('devise.omniauth_callbacks.success', kind: provider.human_name)
         else
           # The user couldn't be created. Most probably the omniauth data
           # didn't have all the required information. Like for example Twitter
           # is not sending email addresses. We need to ask the user to provide
           # the missing details.
-          session[:omniauth] = omni.except('extra')
+          session[:omniauth] = omniauth_data.except('extra')
           redirect_to new_user_registration_path, notice: I18n.t('devise.omniauth_callbacks.provide_missing_details')
         end
       end
